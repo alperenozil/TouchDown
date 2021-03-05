07594e89ebe848c20c8389fdece9bf36bb02fb29
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-03-05 20:56:03 +0300

    update

---
+++ TouchDown/Utility/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  TouchDown
+//
+//  Created by Necdet Alperen Özil on 5.03.2021.
+//
+
+import SwiftUI
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
+
